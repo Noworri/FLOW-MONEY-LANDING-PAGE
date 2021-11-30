adc6be136f0e9bb04be787d8aa61feb8a3dd4e23
emericAz <azonsou158@gmail.com>
2021-11-30 17:16:13 +0100

    fix Language detection

--- src/app/service/translation.service.ts
+++ src/app/service/translation.service.ts
@@ -1,6 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import getUserLocale from 'get-user-locale';
+import { location_data } from '../Location_data.ts/location_data';
 
 @Injectable({
   providedIn: 'root'
@@ -9,21 +11,29 @@ export class TranslationService {
 
   supportedLanguage = ['en', 'fr'];
   userLanguage = '';
+  language = '';
+
+  private url = 'https://ipapi.co/json/'
 
 
 
-  constructor(private translate: TranslateService) {
-    this.initLanguage();
-    this.translate.use(this.userLanguage)
+  constructor(private translate: TranslateService, private http: HttpClient) {
+    setTimeout(() => {
+      this.initLanguage();
+    }, 2000)
   }
-
-
+  getUserLanguage() {
+    this.http.get<location_data>(this.url).subscribe((contryData) => {
+      this.language = contryData.languages.split('-')[0];
+    });
+  }
   initLanguage() {
-    const language = getUserLocale();
-    if (this.supportedLanguage.includes(language)) {
-      this.userLanguage = language;
+    this.getUserLanguage();
+    if (this.supportedLanguage.includes(this.userLanguage)) {
+      this.userLanguage = this.language;
     } else {
-      this.userLanguage = 'en'
+      this.userLanguage = 'en';
     }
+    this.translate.use(this.userLanguage);
   }
 }
